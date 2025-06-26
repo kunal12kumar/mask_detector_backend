c1305ca53fdcde28400b6f301c27a90ca6bdc416
kunal12kumar <23cs3035@rgipt.ac.in>
2025-06-26 07:44:18 +0530

    model loaded successfully

--- app/controllers/detect_controller_image_video.py
+++ app/controllers/detect_controller_image_video.py
@@ -1,1 +1,5 @@
 #in this  we will define the function of proccessing the image and video for the detection 
+import cv2
+import os 
+import uuid
+from ultralytics import yolo
--- app/services/load_trained_model.py
+++ app/services/load_trained_model.py
@@ -1,1 +1,5 @@
 # In this we will load our model and pass the images and videos to give the result 
+# a function to lad the model
+
+from ultralytics import YOLO 
+model=YOLO("best.pt")
